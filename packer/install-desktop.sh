11cdbf1e07cc0a2045a2c63d0e89203d41a41928
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-06-14 16:25:55 -0400

    Include Packer image build with Vagrant tests

---
+++ packer/install-desktop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -
+
+echo "=== install Chrome Remote Desktop ==="
+
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+
+sudo apt update
+
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+
+echo "=== install xfce ==="
+
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
+
+echo "xfce4-session" > ~/.chrome-remote-desktop-session
+
+sudo apt install --assume-yes xscreensaver
+
+echo "=== disable display ==="
+
+sudo systemctl disable lightdm.service
+
+echo "=== install Chrome ==="
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
---
+++ packer/install-devtools.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -
+
+OS="linux"
+ARCH="amd64"
+
+GO_VERSION="1.12.5"
+TERRAFORM_VERSION="0.11.14"
+
+sudo apt install -y git unzip
+git --version
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+echo "=== install Golang ==="
+wget https://dl.google.com/go/go${GO_VERSION}.${OS}-${ARCH}.tar.gz
+sudo tar -C /usr/local -xzf go${GO_VERSION}.${OS}-${ARCH}.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+if [[ $(go version) != *${GO_VERSION}* ]]; then
+  exit 1
+fi
+
+echo "=== install vs code ==="
+wget -O code_amd64.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg --install code_amd64.deb
+sudo apt install --assume-yes --fix-broken
+
+echo "== install terraform ==="
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
+unzip terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
+rm -f terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
+sudo mv terraform /usr/local/bin/
+if [[ $(terraform version) != *${TERRAFORM_VERSION}* ]]; then
+  exit 1
+fi
